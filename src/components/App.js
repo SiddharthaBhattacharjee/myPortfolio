059259a67ad3f08cb0241f3bdbe4259815492961
Siddhartha <siddharthabhatt2345@gmail.com>
2023-03-31 20:42:20 +0530

    Started HomePage | Added Footer

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Routes, Route, NavLink} from 'react-router-dom';
+import { BrowserRouter, Routes, Route, NavLink } from 'react-router-dom';
 import Home from './Home';
 import About from './About';
 import Projects from './Projects';
@@ -10,24 +10,34 @@ import './App.css';
 function App() {
     return (
         <div className='main'>
-            <div className="sub" style={{width:"95%",height:"92%", backgroundColor:"#1E2D3D"}}>
+            <div className="sub" style={{ width: "95%", height: "92%", backgroundColor: "#1E2D3D" }}>
                 <BrowserRouter>
-                    <nav className='navbar'>
+                    <div className='Top'>
+                        <nav className='navbar'>
+                            <div className='innerNavbar'>
+                                <p className="myName">My Portfolio</p>
+                                <NavLink to="/" className='Link' activeClassName="active">Home</NavLink>
+                                <NavLink to="/about" className='Link' activeClassName="active">About-me</NavLink>
+                                <NavLink to="/projects" className='Link' activeClassName="active">Projects</NavLink>
+                            </div>
+                            <NavLink to="/contact" className='Linkouter' activeClassName="active">Contact Me</NavLink>
+                        </nav>
+                        <Routes>
+                            <Route path="/" element={<Home />} />
+                            <Route path="/about" element={<About />} />
+                            <Route path="/projects" element={<Projects />} />
+                            <Route path="/*" element={<NoPage />} />
+                            <Route path="/contact" element={<Contact />} />
+                        </Routes>
+                    </div>
+                    <footer className="footer">
                         <div className='innerNavbar'>
-                            <p className="myName">My Portfolio</p>
-                            <NavLink to="/" className='Link' activeClassName="active">Home</NavLink>
-                            <NavLink to="/about" className='Link' activeClassName="active">About-me</NavLink>
-                            <NavLink to="/projects" className='Link' activeClassName="active">Projects</NavLink>
+                            <p className="myName">Find me in:</p>
+                            <a href="https://twitter.com/Siddhartha_2345" target="_blank" rel="noreferrer" className='socials'><img src={require('./res/twitter.png')} /></a>
+                            <a href="https://www.linkedin.com/in/siddharthabhattacharjee2345/" target="_blank" rel="noreferrer" className='socials'><img src={require('./res/LinkedIn.png')} /></a>
                         </div>
-                        <NavLink to="/contact" className='Linkouter' activeClassName="active">Contact Me</NavLink>
-                    </nav>
-                    <Routes>
-                        <Route path="/" element={<Home />} />
-                        <Route path="/about" element={<About />} />
-                        <Route path="/projects" element={<Projects />} />
-                        <Route path="/*" element={<NoPage />} />
-                        <Route path="/contact" element={<Contact />} />
-                    </Routes>
+                        <a href="https://github.com/SiddharthaBhattacharjee" target="_blank" rel="noreferrer" className='LinkouterFt'>@SiddharthaBhattacharjee <img src={require('./res/GitHub.png')} /></a>
+                    </footer>
                 </BrowserRouter>
             </div>
         </div>
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,53 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import './Home.css';
 
 function Home() {
-  return <h1 style={{color:"white"}}>Welcome to the Home page!</h1>;
+  const skills = [
+    'Full Stack Web Developer',
+    'Android Developer',
+    'Smartcontract Developer',
+    'Public Speaker',
+    'Python Developer'
+  ];
+
+  const [currentSkillIndex, setCurrentSkillIndex] = useState(0);
+  const [currentSkillPosition, setCurrentSkillPosition] = useState(0);
+
+  useEffect(() => {
+    const timeoutId = setTimeout(() => {
+      if (currentSkillPosition === skills[currentSkillIndex].length + 1) {
+        // If the current skill is fully typed, delete it and move on to the next one
+        setCurrentSkillPosition(0);
+        setCurrentSkillIndex((currentSkillIndex + 1) % skills.length);
+      } else if (currentSkillPosition < skills[currentSkillIndex].length) {
+        // If the current skill is not fully typed yet, type one more character
+        setCurrentSkillPosition(currentSkillPosition + 1);
+      } else {
+        // If the current skill is fully deleted, start typing it again
+        setCurrentSkillPosition(skills[currentSkillIndex].length + 1);
+      }
+    }, 200);
+
+    // Clean up the timer when the component is unmounted
+    return () => clearTimeout(timeoutId);
+  }, [currentSkillIndex, currentSkillPosition]);
+
+  const currentSkill = skills[currentSkillIndex].substring(0, currentSkillPosition);
+
+  return (
+    <div className="home">
+      <div className="homeLeft">
+        <p>
+          <section className='Line_1'>Hi all. I am</section><br />
+          <section className='Name_'>SIDDHARTHA BHATTACHARJEE</section><br />
+          <section className='Line_2'> &gt; {currentSkill}<span className="cursor"></span></section>
+          <br/>
+          <br/>
+        </p>
+      </div>
+      <div className="homeRight"></div>
+    </div>
+  );
 }
 
 export default Home;
